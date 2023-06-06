7681da24d96506586b54bea22d7459c815458b45
inmyth <martin.bcu@gmail.com>
2023-06-06 13:02:11 +0900

    Read hex from dump

--- src/main/scala/dump/DumpService.scala
+++ src/main/scala/dump/DumpService.scala
@@ -15,10 +15,11 @@ case class DumpService(filename: String, process: Process) {
   val fileIterator                                     = Source.fromFile(filename, "ISO8859_1").getLines()
   val fileInputStream: ZStream[Any, Throwable, String] = ZStream.fromIterator(fileIterator)
 
-  val p: ZStream[Any, Throwable, Nothing] = fileInputStream.zipWithIndex
+  val p: ZStream[Any, Throwable, Nothing] = fileInputStream
     .mapZIO(p =>
       for {
-        res <- ZIO.fromMonixTask(process.process(p._2, p._1.getBytes("ISO8859_1")))
+        els <- ZIO.succeed(p.split(" ").toVector)
+        res <- ZIO.fromMonixTask(process.process(els(0).toLong, DumpService.hexStringToByteArray(els(1))))
         _   <- ZIO.succeed(println(res))
       } yield ()
     )
@@ -28,6 +29,17 @@ case class DumpService(filename: String, process: Process) {
 }
 
 object DumpService {
+
+  def hexStringToByteArray(s: String): Array[Byte] = {
+    val len = s.length
+    val data = new Array[Byte](len / 2)
+    var i = 0
+    while (i < len) {
+      data(i / 2) = ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16)).toByte
+      i += 2
+    }
+    data
+  }
   def setup(config: Config, filename: String): DumpService = {
     val store = config.dbType match {
       case DbType.redis => Store.redis(config.channel, config.redisConfig)
