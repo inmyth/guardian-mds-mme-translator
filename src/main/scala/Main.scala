667984fe4565fe5f00653bfee19c1d72ef2c3d58
inmyth <martin.bcu@gmail.com>
2023-08-25 13:12:01 +0900

    Chagne Main to run DumpServiceKafka

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ package com.guardian
 import AppError.ConfigError
 
 import cats.implicits.toBifunctorOps
+import com.guardian.dump.DumpServiceKafka
 import com.typesafe.scalalogging.LazyLogging
 import pureconfig.ConfigSource
 import pureconfig.generic.auto._
@@ -18,9 +19,11 @@ object Main extends ZIOAppDefault with LazyLogging {
           .load[Config]
           .leftMap(e => ConfigError(s"Cannot load config: $e"))
       )
-      cons <- ZIO.succeed(ConsumerService.setup(conf))
-      _    <- ZIO.fromMonixTask(cons.connectToStore())
-      _    <- cons.c.runDrain.provide(cons.consumer)
+//      cons <- ZIO.succeed(ConsumerService.setup(conf))
+//      _    <- ZIO.fromMonixTask(cons.connectToStore())
+//      _    <- cons.c.runDrain.provide(cons.consumer)
+      serv <- ZIO.succeed(DumpServiceKafka(conf.filename.get, conf.kafkaConfig.server, conf.kafkaConfig.topic.get))
+      _    <- serv.p.runDrain
     } yield ()
   }
 }
